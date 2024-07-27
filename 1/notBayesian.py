f5b2deb98f3d8e17bba9d73c0dfc9e315e62a8de
kazuki1023 <k.keio12569@gmail.com>
2024-07-27 17:10:49 +0900

    feat: :fire:

--- 1/notBayesian.py
+++ 1/notBayesian.py
@@ -12,9 +12,4 @@ confidence = 1 - alpha
 ci_wilson = stats.binom.interval(confidence, n=n, p=p_hat, loc=0)
 ci_wilson = [ci_wilson[0] / n, ci_wilson[1] / n]
 
-# Clopper-Pearson Intervalの計算
-ci_clopper_pearson = stats.beta.interval(confidence, a=k, b=n-k+1)
-
-# 結果表示
-print(f"Clopper-Pearson Interval: {ci_clopper_pearson}")
 print(f"Wilson Score Interval: {ci_wilson}")
